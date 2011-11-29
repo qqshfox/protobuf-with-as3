3e0ef0c5977003b2a92757b477e50b5d63c7d084
Hanfei Shen <hanfei.shen@saybot.com>
2011-11-29 15:19:33 +0800

    int as3

--- src/google/protobuf/compiler/main.cc
+++ src/google/protobuf/compiler/main.cc
@@ -34,6 +34,7 @@
 #include <google/protobuf/compiler/cpp/cpp_generator.h>
 #include <google/protobuf/compiler/python/python_generator.h>
 #include <google/protobuf/compiler/java/java_generator.h>
+#include <google/protobuf/compiler/as3/as3_generator.h>
 
 
 int main(int argc, char* argv[]) {
@@ -56,6 +57,11 @@ int main(int argc, char* argv[]) {
   google::protobuf::compiler::python::Generator py_generator;
   cli.RegisterGenerator("--python_out", &py_generator,
                         "Generate Python source file.");
+
+  // Proto2 ActionScript
+  google::protobuf::compiler::as3::As3Generator as3_generator;
+  cli.RegisterGenerator("--as3_out", &as3_generator,
+                        "Generate ActionScript source file.");
 
   return cli.Run(argc, argv);
 }
